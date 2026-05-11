c8f04af9a0804d37419a35f787e83355bdf2021d
Suryansh <suryansh.business.work@gmail.com>
2026-05-11 14:43:22 +0530

    Change Date

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -34,6 +34,7 @@ export const GET: APIRoute = ({ url, site }) => {
     .join('\n');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`;
